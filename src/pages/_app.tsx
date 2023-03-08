717f88c98964c91085ec655e1daa98be0406b7c0
MartinZavadzan <martin.zavadzan@vacuumlabs.com>
2023-03-08 10:18:58 +0100

    rainbowkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../../next-i18next.config.js'
-import { ConnectWalletButton } from '@/components/wallet/ConnectWalletButton'
+import { RainbowKitProvider, ConnectButton } from '@rainbow-me/rainbowkit'
 import React from 'react'
 import { WagmiConfig } from 'wagmi'
-import { client } from '../wagmiConfig'
+import { wagmiClient, chains } from '../wagmiConfig'
+import '@rainbow-me/rainbowkit/styles.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  console.log(process.env.NEXT_PUBLIC_ENABLE_TESTNETS)
+
   return (
-    <WagmiConfig client={client}>
-      <ConnectWalletButton />
-      <title>Wolf Society</title>
-      <Component {...pageProps} />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectButton />
+        <title>Wolf Society</title>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
     </WagmiConfig>
   )
 }
--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,27 +1,28 @@
-import { configureChains, createClient, mainnet } from 'wagmi'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { configureChains, createClient, mainnet, goerli } from 'wagmi'
+import { sepolia } from 'wagmi/chains'
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { publicProvider } from 'wagmi/providers/public'
 
-const { chains, provider } = configureChains([mainnet], [publicProvider()])
-
-export const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wolf-society',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    mainnet,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+      ? [goerli, sepolia]
+      : []),
   ],
-  provider,
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Wolf-society',
+  chains,
 })
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+})
+
+export { chains, wagmiClient }
