b2ebb1a7b5fc35df89ad22a7929d16de9f6600c9
MartinZavadzan <martin.zavadzan@vacuumlabs.com>
2023-03-10 16:16:12 +0100

    fix build issue

--- src/utils/hooks/useGetNfts.ts
+++ src/utils/hooks/useGetNfts.ts
@@ -3,15 +3,13 @@ import { useEffect, useState } from 'react'
 import { alchemy } from '../configs/alchemy'
 
 export const useGetNfts = (address: string | undefined): Nft[] => {
-  if (!address) return []
   const [nfts, setNfts] = useState<Nft[]>([])
-
   const fetchNfts = async (address: string) => {
     setNfts((await alchemy.nft.getNftsForOwner(address)).ownedNfts)
   }
 
   useEffect(() => {
-    fetchNfts(address)
+    if (address) fetchNfts(address)
   }, [])
 
   return nfts
