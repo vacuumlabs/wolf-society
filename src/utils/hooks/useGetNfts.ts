429687b8a8e9893ad4068352083246ac0a9f63c3
MartinZavadzan <martin.zavadzan@vacuumlabs.com>
2023-03-14 12:45:35 +0100

    suggestions from rewiev

--- src/utils/hooks/useGetNfts.ts
+++ src/utils/hooks/useGetNfts.ts
@@ -4,13 +4,13 @@ import { alchemy } from '../configs/alchemy'
 
 export const useGetNfts = (address: string | undefined): Nft[] => {
   const [nfts, setNfts] = useState<Nft[]>([])
-  const fetchNfts = async (address: string) => {
-    setNfts((await alchemy.nft.getNftsForOwner(address)).ownedNfts)
-  }
 
   useEffect(() => {
+    const fetchNfts = async (address: string) => {
+      setNfts((await alchemy.nft.getNftsForOwner(address)).ownedNfts)
+    }
     if (address) fetchNfts(address)
-  }, [])
+  }, [address])
 
   return nfts
 }
