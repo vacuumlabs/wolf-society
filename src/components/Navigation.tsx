8cf49ce0b2e494f012607075e44c474e14557ace
Matej Poklemba <matejpoklemba@gmail.com>
2023-03-17 14:12:55 +0100

    Add logo (png for now) and fix navigation width

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@ import {
   AppBar,
   Box,
   Button,
+  Container,
   IconButton,
   Link,
   Menu,
@@ -15,6 +16,8 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { Translations, useTranslations } from '@/utils/hooks/useTranslations'
 import MenuIcon from '@mui/icons-material/Menu'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+import logoImage from 'public/images/logo.png'
 
 const subpages: { text: keyof Translations; href: string }[] = [
   { text: 'donate', href: '/' },
@@ -40,81 +43,83 @@ const Navigation = () => {
       style={{ boxShadow: 'none' }}
       sx={(theme) => ({ fontSize: theme.typography.button.fontSize, py: 2 })}
     >
-      <Toolbar>
-        <Typography variant="h6">{t('wolfSocietyFoundation')}</Typography>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          gap={2}
-          sx={{
-            display: { mobile: 'none', tablet: 'flex' },
-            flexGrow: 1,
-          }}
-        >
-          {subpages.map((subpage) => {
-            const isCurrentSubpage = router.pathname === subpage.href
-            const color = isCurrentSubpage ? 'primary' : 'inherit'
-            return (
-              <Button key={subpage.text} href={subpage.href} color={color}>
-                {t(subpage.text)}
-              </Button>
-            )
-          })}
-        </Stack>
-        <Box
-          sx={{
-            display: { mobile: 'none', tablet: 'flex' },
-          }}
-        >
-          <ConnectButton />
-        </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-            justifyContent: 'end',
-            display: { mobile: 'flex', tablet: 'none' },
-          }}
-        >
-          <IconButton
-            size="large"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-            color="inherit"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
+      <Container>
+        <Toolbar>
+          <Image src={logoImage} alt="Logo" height="48" />
+          <Stack
+            direction="row"
+            justifyContent="center"
+            gap={2}
             sx={{
-              display: { mobile: 'block', tablet: 'none' },
+              display: { mobile: 'none', tablet: 'flex' },
+              flexGrow: 1,
             }}
           >
-            {subpages.map((subpage) => (
-              <MenuItem key={subpage.text} onClick={handleCloseNavMenu}>
-                <Link textAlign="center" href={subpage.href} underline="none">
+            {subpages.map((subpage) => {
+              const isCurrentSubpage = router.pathname === subpage.href
+              const color = isCurrentSubpage ? 'primary' : 'inherit'
+              return (
+                <Button key={subpage.text} href={subpage.href} color={color}>
                   {t(subpage.text)}
-                </Link>
+                </Button>
+              )
+            })}
+          </Stack>
+          <Box
+            sx={{
+              display: { mobile: 'none', tablet: 'flex' },
+            }}
+          >
+            <ConnectButton />
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'end',
+              display: { mobile: 'flex', tablet: 'none' },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { mobile: 'block', tablet: 'none' },
+              }}
+            >
+              {subpages.map((subpage) => (
+                <MenuItem key={subpage.text} onClick={handleCloseNavMenu}>
+                  <Link textAlign="center" href={subpage.href} underline="none">
+                    {t(subpage.text)}
+                  </Link>
+                </MenuItem>
+              ))}
+              <MenuItem key="connectWalletButton" onClick={handleCloseNavMenu}>
+                <ConnectButton />
               </MenuItem>
-            ))}
-            <MenuItem key="connectWalletButton" onClick={handleCloseNavMenu}>
-              <ConnectButton />
-            </MenuItem>
-          </Menu>
-        </Box>
-      </Toolbar>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
