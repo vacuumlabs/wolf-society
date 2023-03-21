04c0dc6db10bcd9afbc07576051e94f605139030
Matej Poklemba <matejpoklemba@gmail.com>
2023-03-21 10:15:27 +0100

    Add Topics section

---
+++ src/components/landing/TopicCard.tsx
@@ -0,0 +1,41 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from '@mui/material'
+import ArrowForward from '@mui/icons-material/ArrowForward'
+
+export type TopicCardProps = {
+  title: string
+  date: string
+  imageUrl: string
+}
+
+const TopicCard = ({ title, date, imageUrl }: TopicCardProps) => {
+  return (
+    <Card sx={{ maxWidth: 404, width: '100%' }}>
+      <CardMedia component="img" height="300" image={imageUrl} alt="hmm" />
+      <CardContent sx={{ bgcolor: 'neutral.main', p: 4, textAlign: 'start' }}>
+        <Stack spacing={4}>
+          <Typography variant="body2">{date}</Typography>
+          <Typography variant="h3">{title}</Typography>
+        </Stack>
+      </CardContent>
+      <CardActions sx={{ padding: 0 }}>
+        <Button
+          color="primary"
+          variant="contained"
+          sx={{ width: '100%' }}
+          endIcon={<ArrowForward />}
+        >
+          Read More
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+export default TopicCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import { NftCard } from '@/components/NftCard'
 import MakeImpact from '@/components/landing/MakeImpact'
 import Roadmap from '@/components/landing/Roadmap'
 import Questions from '@/components/landing/Questions'
+import Topics from '@/components/landing/Topics'
 
 const Home = () => {
   const t = useTranslations()
@@ -26,6 +27,7 @@ const Home = () => {
       <Collections />
       <Roadmap />
       <Questions />
+      <Topics />
     </Stack>
   )
 }
