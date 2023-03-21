4bb86c2f95d1952f54a811f173cafeb6039c1028
Matej Poklemba <matejpoklemba@gmail.com>
2023-03-21 14:39:18 +0100

    Minor cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,9 @@ import Collections from '@/components/landing/Collections'
 import Hero from '@/components/landing/Hero'
 import Manifest from '@/components/landing/Manifest'
 import Projects from '@/components/landing/Projects'
-import {
-  injectTranslations,
-  useTranslations,
-} from '@/utils/hooks/useTranslations'
+import { injectTranslations } from '@/utils/hooks/useTranslations'
 import { Stack } from '@mui/material'
 import { GetStaticPropsContext } from 'next'
-import { useAccount } from 'wagmi'
-import { useGetNfts } from '@/utils/hooks/useGetNfts'
-import { NftCard } from '@/components/NftCard'
 import MakeImpact from '@/components/landing/MakeImpact'
 import Roadmap from '@/components/landing/Roadmap'
 import Questions from '@/components/landing/Questions'
@@ -18,7 +12,6 @@ import Topics from '@/components/landing/Topics'
 import CTA from '@/components/landing/CTA'
 
 const Home = () => {
-  const t = useTranslations()
   return (
     <Stack>
       <Hero />
