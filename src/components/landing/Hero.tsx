f42a5006171ad81e8770a646f5b49b008bd039ad
MartinZavadzan <martin.zavadzan@vacuumlabs.com>
2023-04-03 12:34:50 +0200

    hero image parallax

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -11,10 +11,18 @@ import {
   Theme,
 } from '@mui/material'
 import Image from 'next/image'
-import heroImage from 'public/images/hero.png'
+import circleImage from 'public/images/circle.png'
+import factoryImage from 'public/images/factory.png'
+import handsImage from 'public/images/hands.png'
+import mountainImage from 'public/images/mountain.png'
+import peopleImage from 'public/images/people.png'
 import React from 'react'
 import AppearingComponent from '../AppearingComponent'
 import Button from '../Button'
+import {
+  MouseParallaxChild,
+  MouseParallaxContainer,
+} from 'react-parallax-mouse'
 
 type Props = {
   manifestRef: React.RefObject<HTMLElement>
@@ -68,26 +76,14 @@ const Hero = ({ manifestRef }: Props) => {
       </AppearingComponent>
       <AppearingComponent>
         <Box
-          sx={{
-            height: 'auto',
+          style={{
+            height: '45vw',
             position: 'relative',
             maxHeight: '1000px',
+            width: '100%',
+            overflow: 'hidden',
           }}
         >
-          <Image
-            src={heroImage}
-            alt="Hero image"
-            priority
-            style={{
-              objectFit: isMobile ? 'cover' : 'contain',
-              objectPosition: 'top',
-              position: 'relative',
-              height: isMobile ? '365px' : '100%',
-              width: '100%',
-              zIndex: 1,
-              maxHeight: '1000px',
-            }}
-          />
           <Box
             bgcolor="secondary.main"
             height="27%"
@@ -95,6 +91,100 @@ const Hero = ({ manifestRef }: Props) => {
             width="100%"
             bottom="0"
           />
+          <MouseParallaxContainer
+            globalFactorX={0.1}
+            globalFactorY={0}
+            resetOnLeave
+            containerStyle={{
+              position: 'static',
+              width: '100%',
+              height: '100%',
+              overflowX: 'hidden',
+            }}
+          >
+            <MouseParallaxChild
+              factorX={0.2}
+              style={{ position: 'absolute', zIndex: 91, top: 0, right: 0 }}
+            >
+              <Image
+                src={mountainImage}
+                alt="Mountain image"
+                style={{
+                  width: '34.5vw',
+                  height: 'auto',
+                }}
+              />
+            </MouseParallaxChild>
+            <MouseParallaxChild
+              factorX={0.1}
+              style={{ position: 'absolute', zIndex: 91, top: '6.5%', left: 0 }}
+            >
+              <Image
+                src={factoryImage}
+                alt="Factory image"
+                style={{
+                  width: '37.5vw',
+                  height: 'auto',
+                }}
+              />
+            </MouseParallaxChild>
+
+            <MouseParallaxChild
+              factorX={0.1}
+              style={{
+                position: 'absolute',
+                zIndex: 90,
+                top: '5%',
+                left: '46.5%',
+              }}
+            >
+              <Image
+                src={handsImage}
+                alt="Hands image"
+                style={{
+                  width: '47vw',
+                  height: 'auto',
+                }}
+              />
+            </MouseParallaxChild>
+            <MouseParallaxChild
+              factorX={0}
+              style={{
+                position: 'absolute',
+                zIndex: 0,
+                top: '19%',
+                left: '28%',
+              }}
+            >
+              <Image
+                src={circleImage}
+                alt="Circle image"
+                style={{
+                  width: '45vw',
+                  height: 'auto',
+                }}
+              />
+            </MouseParallaxChild>
+            <MouseParallaxChild
+              factorX={0.3}
+              inverted
+              style={{
+                position: 'absolute',
+                zIndex: 92,
+                top: '26.5%',
+                left: '16.5%',
+              }}
+            >
+              <Image
+                src={peopleImage}
+                alt="People image"
+                style={{
+                  width: '59vw',
+                  height: 'auto',
+                }}
+              />
+            </MouseParallaxChild>
+          </MouseParallaxContainer>
         </Box>
       </AppearingComponent>
     </Box>
