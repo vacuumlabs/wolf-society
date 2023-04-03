723773639468feacbed8205320d3a12e4a5e8838
Matej Poklemba <matejpoklemba@gmail.com>
2023-04-03 09:47:27 +0200

    Make a copy of collections page (code will be used in account page)

---
+++ src/pages/account.tsx
@@ -0,0 +1,45 @@
+import {
+  ContentTypes,
+  injectCMSContent,
+  useContentful,
+} from '@/utils/hooks/useContentful'
+import { GetStaticPropsContext } from 'next'
+import { useAccount } from 'wagmi'
+import { useGetNfts } from '@/utils/hooks/useGetNfts'
+import { NftCard } from '@/components/NftCard'
+import { useGetNftsCollections } from '@/utils/hooks/useGetNftsCollection'
+import { ourCollectionsAddresses } from '@/consts'
+import { compareNfts } from '@/utils/helpers'
+
+const Collections = () => {
+  const translate = useContentful(ContentTypes.collectionsPage)
+  const { address } = useAccount()
+  const ownedNfts = useGetNfts(address)
+  const wlfSocietyNfts = useGetNftsCollections(ourCollectionsAddresses)
+
+  return (
+    <>
+      {wlfSocietyNfts?.map((nft, index) => (
+        <NftCard
+          key={index}
+          nft={nft}
+          owned={ownedNfts.some((ownedNft) => compareNfts(ownedNft, nft))}
+        />
+      ))}
+    </>
+  )
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    // Will be passed to the page component as props
+    props: {
+      translations: await injectCMSContent(
+        ContentTypes.collectionsPage,
+        locale
+      ),
+    },
+  }
+}
+
+export default Collections
