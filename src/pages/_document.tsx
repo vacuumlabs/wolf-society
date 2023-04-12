c6b62891728e4ac98d91708094c6c783725b524a
MartinZavadzan <martin.zavadzan@vacuumlabs.com>
2023-04-12 11:04:23 +0200

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,26 @@ import React from 'react'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
