e6f1947302053116ff96ec1d0770cbbad2cdb666
MartinZavadzan <martin.zavadzan@vacuumlabs.com>
2023-04-14 10:49:42 +0200

    fix favicon on firefox

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,23 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
+          id="favicon"
           rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
+          href="/images/favicon.ico"
+          type="image/x-icon"
         />
       </Head>
       <body>
