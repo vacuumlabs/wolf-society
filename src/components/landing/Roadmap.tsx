ddb24d8e3a01c5f3a8c6a060235ec7003cb29f78
Matej Poklemba <matejpoklemba@gmail.com>
2023-05-08 10:37:28 +0200

    Add Roadmap heading

--- src/components/landing/Roadmap.tsx
+++ src/components/landing/Roadmap.tsx
@@ -3,8 +3,15 @@ import {
   RoadmapData,
   useContentful,
 } from '@/utils/hooks/useContentful'
-import { Box, Container, Stack, Theme, useMediaQuery } from '@mui/material'
-import { HorizontalScrollText } from './HorizontalScrollText'
+import {
+  Box,
+  BreakpointOverrides,
+  Container,
+  Stack,
+  Theme,
+  Typography,
+  useMediaQuery,
+} from '@mui/material'
 import RoadmapCard, { RoadmapCardProps } from './RoadmapCard'
 import { RoadmapDesktop } from './RoadmapDesktop'
 import { SECTIONS } from '@/consts'
@@ -48,8 +55,9 @@ type Props = {
 
 const Roadmap = ({ roadmapData }: Props) => {
   const translate = useContentful(ContentTypes.landingPage)
+  const breakpoint: keyof BreakpointOverrides = 'tabletM'
   const isMobile = useMediaQuery((theme: Theme) =>
-    theme.breakpoints.down('tabletM')
+    theme.breakpoints.down(breakpoint)
   )
   if (!roadmapData) return null
 
@@ -60,26 +68,31 @@ const Roadmap = ({ roadmapData }: Props) => {
       zIndex={10}
     >
       <Container>
-        {isMobile ? (
-          <Stack sx={{ alignItems: 'center', my: 10 }} spacing={5}>
-            {roadmapData.map((roadmapItem, index) => (
-              <RoadmapCard
-                key={`${roadmapItem.quarter}-${roadmapItem.year}`}
-                color={COLOR_ORDER[index % COLOR_ORDER.length]}
-                quarter={roadmapItem.quarter}
-                year={roadmapItem.year}
-                items={roadmapItem.items.split('\n')}
-              />
-            ))}
-          </Stack>
-        ) : (
-          <RoadmapDesktop
-            roadmapData={roadmapData.map((data, idx) => ({
-              color: COLOR_ORDER[idx % COLOR_ORDER.length],
-              ...data,
-            }))}
-          />
-        )}
+        <Stack textAlign="center" my={{ mobile: 10, [breakpoint]: 20 }} gap={5}>
+          <Typography variant="display" sx={{ fontWeight: 600 }}>
+            {translate('roadmap')}
+          </Typography>
+          {isMobile ? (
+            <Stack sx={{ alignItems: 'center' }} spacing={5}>
+              {roadmapData.map((roadmapItem, index) => (
+                <RoadmapCard
+                  key={`${roadmapItem.quarter}-${roadmapItem.year}`}
+                  color={COLOR_ORDER[index % COLOR_ORDER.length]}
+                  quarter={roadmapItem.quarter}
+                  year={roadmapItem.year}
+                  items={roadmapItem.items.split('\n')}
+                />
+              ))}
+            </Stack>
+          ) : (
+            <RoadmapDesktop
+              roadmapData={roadmapData.map((data, idx) => ({
+                color: COLOR_ORDER[idx % COLOR_ORDER.length],
+                ...data,
+              }))}
+            />
+          )}
+        </Stack>
       </Container>
     </Box>
   )
