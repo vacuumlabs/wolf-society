640e4f869c2f9696529e21b3bb50087d9fb3889d
Samuel Šulovský <samuel.sulovsky@gmail.com>
2023-05-17 17:05:14 +0200

    Update DB Schema to reflect reqs

--- db/schema.sql
+++ db/schema.sql
@@ -5,38 +5,28 @@ CREATE TABLE IF NOT EXISTS app_user (
   PRIMARY KEY(eth_address)
 );
 
-CREATE TABLE IF NOT EXISTS collection (
-  id SERIAL,
-  reward_amount INTEGER DEFAULT 0,
-  PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS task_group (
+  name VARCHAR(42), -- Unique NFT Address OR name (for the sitewide tasks)
+  PRIMARY KEY(name)
 );
 
 CREATE TABLE IF NOT EXISTS task (
-  id SERIAL,
-  collection_id INTEGER NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(collection_id) REFERENCES collection(id)
+  id INTEGER,
+  task_group_name VARCHAR(42) NOT NULL,
+  reward_amount INTEGER DEFAULT 0,
+  PRIMARY KEY(id, task_group_name),
+  FOREIGN KEY(task_group_name) REFERENCES task_group(name)
 );
 
 CREATE TABLE IF NOT EXISTS completed_task (
-  collection_id INTEGER,
+  task_group_name VARCHAR(42),
   task_id INTEGER,
   completed_by VARCHAR(42),
   completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-  PRIMARY KEY(collection_id, task_id, completed_by),
-  FOREIGN KEY(collection_id) REFERENCES collection(id),
+  PRIMARY KEY(task_group_name, task_id, completed_by),
+  FOREIGN KEY(task_group_name) REFERENCES task_group(name)
   FOREIGN KEY(task_id) REFERENCES task(id),
-  FOREIGN KEY(completed_by) REFERENCES app_user(eth_address)
-);
-
-CREATE TABLE IF NOT EXISTS completed_collection (
-  collection_id INTEGER,
-  completed_by VARCHAR(42),
-  completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-  PRIMARY KEY(collection_id, completed_by),
-  FOREIGN KEY(collection_id) REFERENCES collection(id),
   FOREIGN KEY(completed_by) REFERENCES app_user(eth_address)
 );
 
@@ -47,8 +37,9 @@ DECLARE
   points INTEGER;
 BEGIN
   SELECT reward_amount INTO points 
-    FROM collection
-    WHERE id = NEW.collection_id;
+    FROM task
+    WHERE id = NEW.task_id 
+    AND task_group_name = NEW.task_group_name;
 
   UPDATE app_user
     SET reward_points = reward_points + points
@@ -60,7 +51,7 @@ $t$ LANGUAGE PLPGSQL;
 
 CREATE OR REPLACE TRIGGER award_points_trigger
 AFTER INSERT 
-ON completed_collection
+ON completed_task
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_function();
 
