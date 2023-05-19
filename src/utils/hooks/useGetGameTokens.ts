4a25306fa8db96b3b96ca1f213542dbc32721d96
Samuel Šulovský <samuel.sulovsky@gmail.com>
2023-05-19 12:08:01 +0200

    Fetch user's game tokens from BE

---
+++ src/utils/hooks/useGetGameTokens.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import { useAccount } from 'wagmi'
+
+export const useGetGameTokens = () => {
+  const [gameTokens, setGameTokens] = useState<number | undefined>(undefined)
+  const { address } = useAccount()
+
+  useEffect(() => {
+    console.log('effect fired with', address)
+    const fetchBalance = async (address: string) => {
+      const res = await fetch(`/api/user/${address}`)
+      const { points } = await res.json()
+
+      setGameTokens(points)
+    }
+
+    if (address == null) {
+      return undefined
+    }
+
+    fetchBalance(address)
+  }, [address])
+
+  return gameTokens
+}
