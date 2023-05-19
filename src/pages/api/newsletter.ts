8d743beeae436a5ab6fe64d07664c33aa0cc736f
Matej Poklemba <matejpoklemba@gmail.com>
2023-05-19 12:25:24 +0200

    Code review changes

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -2,10 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Client } from '@hubspot/api-client'
 import { SimplePublicObjectInput } from '@hubspot/api-client/lib/codegen/crm/contacts'
 
+export type ResponseData = {
+  message: string
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ResponseData>
 ) {
+  if (req.method !== 'POST')
+    res.status(400).json({ message: 'Invalid request method!' })
   try {
     const hubspotClient = new Client({
       accessToken: process.env.HUBSPOT_ACCESS_TOKEN,
@@ -17,14 +23,14 @@ export default async function handler(
       },
     }
     await hubspotClient.crm.contacts.basicApi.create(contactObj)
-    res.status(200).json({})
+    res.json({ message: 'E-mail successfully subscribed to the newsletter!' })
   } catch (err: any) {
-    let errorMessage = 'Unexpected error occured.'
+    let errorMessage = 'Unexpected error occured!'
     switch (err.code) {
       case 409:
         errorMessage = 'E-mail already registered!'
         break
     }
-    res.status(err.code).json({ error: errorMessage })
+    res.status(err.code).json({ message: errorMessage })
   }
 }
