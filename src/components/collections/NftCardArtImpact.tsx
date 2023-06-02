d2bae373d334a5796e4dad7e26bd84892157a801
Matej Poklemba <matejpoklemba@gmail.com>
2023-06-02 12:19:04 +0200

    Replace framer-motion library by OnScreen (#156)

--- src/components/collections/NftCardArtImpact.tsx
+++ src/components/collections/NftCardArtImpact.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mui/material'
-import { useEffect, useRef } from 'react'
-import dynamic from 'next/dynamic'
-import { useInView } from 'framer-motion'
 import NftCard, { NftCardProps } from '../NftCard'
+import { OnScreen } from '../OnScreen'
 
 export type NftCardArtImpactProps = {
   changeArtist: () => void
@@ -11,39 +9,37 @@ export type NftCardArtImpactProps = {
   setPointerOver: (value: boolean) => void
 }
 
-const DynamicShareButton = dynamic(
-  () => import('./ShareButton').then((mod) => mod.ShareButton),
-  { ssr: false }
-)
-
 const NftCardArtImpact = ({
   changeArtist,
   isLast,
   nftCardProps,
   setPointerOver,
 }: NftCardArtImpactProps) => {
-  const containerRef = useRef(null)
-  const isInView = useInView(containerRef)
-
-  useEffect(() => {
-    if (isInView) changeArtist()
-  }, [isInView, changeArtist])
-
   return (
-    <Box
-      ref={containerRef}
-      onPointerEnter={() => {
-        setPointerOver(true)
-      }}
-      onPointerLeave={() => {
-        setPointerOver(false)
-      }}
-      sx={{
-        mb: { mobile: '0', tabletM: isLast ? 'calc(100vh - 80px)' : '130vh' },
-      }}
-    >
-      <NftCard {...nftCardProps} displayPrice />
-    </Box>
+    <>
+      <Box
+        className={`nftCard${nftCardProps.nftData.id}`}
+        onPointerEnter={() => {
+          setPointerOver(true)
+        }}
+        onPointerLeave={() => {
+          setPointerOver(false)
+        }}
+        sx={{
+          mb: { mobile: '0', tabletM: isLast ? 'calc(100vh - 80px)' : '130vh' },
+        }}
+      >
+        <NftCard {...nftCardProps} displayPrice />
+      </Box>
+      <OnScreen
+        selector={`.nftCard${nftCardProps.nftData.id}`}
+        setIntersecting={(visible: boolean) => {
+          if (visible) {
+            changeArtist()
+          }
+        }}
+      />
+    </>
   )
 }
 export default NftCardArtImpact
